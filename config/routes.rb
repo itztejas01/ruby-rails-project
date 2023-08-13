3583989ad1b214f12c60750598ab0d44644da8b7
Tejas Chaplot <tejas.chaplot25@gmail.com>
2023-08-13 12:36:39 +0530

    added rails project with two routes

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "main#index"
+  
+  get "about-us", to: "about#index", as: :about
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
